55c373d7dc3209e59cc7c911a4e006bf18b870f3
Nishant1810 <gaur18nishant@gmail.com>
2026-06-10 12:20:21 +0530

    Update server

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,27 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("1. ENV Loaded");
+// console.log("1. ENV Loaded");
 
 import http from "http";
 
-console.log("2. HTTP Imported");
+// console.log("2. HTTP Imported");
 
 import app from "./app.js";
 
-console.log("3. App Imported");
+// console.log("3. App Imported");
 
 import connectDB from "./config/db.js";
 
-console.log("4. DB Imported");
+// console.log("4. DB Imported");
 
 connectDB();
 
-console.log("5. DB Function Called");
+// console.log("5. DB Function Called");
 
 const server = http.createServer(app);
 
-console.log("6. Server Created");
+// console.log("6. Server Created");
 
 const PORT = process.env.PORT || 5000;
 
